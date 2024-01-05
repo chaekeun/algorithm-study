19c6ca12ccc66238ce1e9260102e99845337f55f
chaekeun <chaen328@khu.ac.kr>
2024-01-05 17:15:05 +0900

    0105 feat: studying dfs

---
+++ study_14502.py
@@ -0,0 +1,45 @@
+# dfs 동작과정
+
+# 1. 탐색 시작 노드를 스택에 삽입하고 방문처리를 한다.
+# 2. 스택의 최상단 노드에 방문하지 않은 인접노드가 있으면 그 인접 노드를 스택에 넣고 방문처리를 한다. 방문하지 않은 인접 노드가 없으면 스택에서 최상위 노드를 꺼낸다.
+# 3. 2번 과정을 더이상 수행할 수 없을때까지 반복한다.
+
+def dfs(graph, v, visited):
+    #v는 시작위치
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i , visited)
+
+graph = [
+    [],
+    [2,3,7],
+    [1,4,6],
+    [1,5, 7],
+    [2,6],
+    [3,7],
+    [2,4],
+    [1,3]
+]
+
+#각 노드가 방문한 정보를 리스트 자료형으로 표현
+visited = [False] * 9
+
+print("방문순서")
+dfs(graph, 1, visited)
+
+#2차원 배열인 경우
+
+def dfs(x, y):
+
+    visited[x][y] = True
+    print((x, y), end=' ')
+
+    for dx, dy in (-1,0), (1,0), (0,-1), (0,1):
+        nx, ny = x+dx, y+dy
+        if nx<0 or nx>=n or ny<0 or ny>m:
+            continue
+        if not(visited[nx][ny]):
+            dfs(nx, ny)
