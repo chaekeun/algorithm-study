39c7a6ef25fc2302eba061f2f4e5b1b528ef50f9
chaekeun <chaen328@khu.ac.kr>
2024-01-07 17:30:04 +0900

    0107 feat: 계속 써보면서 연습하기

--- study_14502.py
+++ study_14502.py
@@ -43,3 +43,55 @@ def dfs(x, y):
             continue
         if not(visited[nx][ny]):
             dfs(nx, ny)
+
+# def dfs(x, y):
+
+#     countVirus = 1
+
+#     visited[x][y] = True
+
+#     for dx, dy in (-1,0), (1,0), (0,-1), (0,1):
+#         nx, ny = x+dx, y+dy
+
+#         if nx<0 or ny<0 or nx>=n or ny>=m:
+#             continue 
+
+#         if not(visited[nx][ny] or a[nx][ny]):
+#             countVirus+=dfs(nx, ny)
+
+#     return countVirus
+
+
+# def solve(wall, x, y):
+#     global virus, c
+
+#     if wall == 3:
+#         cnt = 0 
+#         visited = [[False]*m for _ in range(n)]
+
+#         for i, j in v:
+#             cnt+=dfs(i, j)
+
+#         virus = min(virus, cnt)
+#         return
+
+#     for i in range(x, n):
+
+#         k=y if i==x else 0
+
+
+#         for j in range(k,m):
+#             if a[i][j]==0:
+#                 a[i][j] = 1
+#                 solve(wall+1, i, j+1)
+#                 a[i][j]=0
+
+# for i in range(n):
+#     for j in range(m):
+#         if a[i][j]==1:
+#             safe += 1
+
+#         if a[i][j]==2:
+#             v.append((i,j))
+
+            
