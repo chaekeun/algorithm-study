56bb401ba904bab5f9e422a12504c07187df36ce
chaekeun <chaen328@khu.ac.kr>
2024-01-07 17:29:46 +0900

    0107 feat: add 설명

--- BOJ_14502.py
+++ BOJ_14502.py
@@ -3,10 +3,19 @@
 #if (countWall = 3) then DFS(if(0) then (2) and countVirus++)
 #answer = max(N*M-countVirus)
 
+#연구실 지도 크기
 n, m = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)] #연구소 지도 모양을 1차원 array에 저장하는듯
-visited = [[False]*m for _ in range(n)] #방문 여부를 나타내는 2차원 배열 (dfs)
-v, safe, virus = [], -3, 100 #v는 폭탄의 좌표값 튜플을 요소로 갖는 list
+#연구실 지도 모양 저장
+a = [list(map(int, input().split())) for _ in range(n)] 
+#방문 여부를 나타내는 2차원 배열 (dfs)
+visited = [[False]*m for _ in range(n)] 
+#v는 폭탄의 좌표값 튜플을 요소로 갖는 list
+v = []
+#안전한 영역의 수 초기화
+safe = -3
+#감염된 영역의 수 초기화
+virus = 100
+
 
 
 #기본 dfs에 res와 a[nx][ny]에 대한 조건 추가한 함수
@@ -15,8 +24,10 @@ def dfs(x, y):
     #x,y는 시작위치
 
     countVirus = 1 #?
+    
     visited[x][y] = True 
-    for dx, dy in (-1,0), (1,0), (0,-1), (0,1): #상하좌우 이동을 위한 반복문
+
+    for dx, dy in (-1,0), (1,0), (0,-1), (0,1): #좌우하상(?)이동을 위한 반복문
         nx, ny = x+dx, y+dy 
         if nx<0 or nx>=n or ny<0 or ny>=m:
             continue #범위를 벗어나면 다음 반복으로 넘어간다
@@ -28,15 +39,18 @@ def solve(wall, x, y):
     global virus, c
     if wall == 3:
         cnt = 0
+        #이런 초기화방식 익숙해지기
         visited = [[False]*m for _ in range(n)] #방문여부 초기화(backtracking)
         for i, j in v: #폭탄의 좌표값들을 돌면서
-            cnt += dfs(i, j) #인접 새롭게 감염된 virus들 count
+            cnt += dfs(i, j) #새롭게 감염된 것까지 포함된 전체 virus들 count
         virus = min(virus, cnt) #가장 작게 감염되었을때를 찾는다
         return 
 
     for i in range(x, n): #행순회
         #2중루프를 위한 k 초기화
-        k = y if i == x else 0
+        #솔직히 왜 이렇게 초기화하는지는 잘 모르겠음..
+        k = y if i == x else 0 #x행일때만 y열부터 시작하고 나머지는 0부터 순회
+        #x행 y열 이전은 부모함수(?)일 것이므로
         for j in range(k, m): #열순회
             if a[i][j] == 0: #빈공간이면
                 a[i][j] = 1 #일단 벽을 세워보고
