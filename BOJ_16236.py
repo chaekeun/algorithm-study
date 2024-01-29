36e9f1f1ef7f6d5e8e078b2bf0e393cdf6ade772
chaekeun <chaen328@khu.ac.kr>
2024-01-29 14:05:33 +0900

    0129 feat: edit boj 16236

--- BOJ_16236.py
+++ BOJ_16236.py
@@ -3,16 +3,16 @@ from heapq import heappush, heappop
 input = stdin.readline
 
 n = int(input())
-a = [list(map(int, input().split())) for _ in range(n)] #결과 확인해보기
+a = [list(map(int, input().split())) for _ in range(n)] #입력값은 이런식으로 저장하는거 익숙해지기
 q = []
 
 def init():
     for i in range(n):
         for j in range(n):
-            if a[i][j] == 9: 
+            if a[i][j] == 9: #상어가 있는 곳을 찾아서
                 heappush(q, (0,i,j))
-                # (distance, i, j)의 튜플을 요소에 추가
-                a[i][j]=0
+                # (이동거리, 행좌표, 열좌표)의 튜플을 요소에 추가
+                a[i][j] = 0 #이거 왜해주는지 잘 모르겠긴함
                 return
 
 def bfs():
@@ -20,25 +20,28 @@ def bfs():
     check = [[False]*n for _ in range(n)]
     while q:
         d,x,y = heappop(q)
-        if 0 < a[x][y] < body:
+        if 0 < a[x][y] < body: #상어보다 작은 물고기가 있는 경우
             eat += 1
             a[x][y]=0
             if eat == body:
                 body += 1
-                eat = 0
-            ans += d # 이동한 거리 누적
+                eat = 0 # 먹은 물고기수 초기화해줘야함
+            ans += d # 먹은 순간까지 이동한 거리 누적
             d = 0 # 초기화
             while q:
-                q.pop() #큐에 들어있는 좌표를 비우는 이유를 잘 모르겠다..
-            check = [[False]*n for _ in range(n)] #다음 물고리를 먹기 위해 움직일 때 이미 갔던 곳을 다시 방문할 수도 있으므로 방문 체크 배열 모두 초기화
-        for dx, dy in (-1,0),(0,-1),(1,0),(0,1):
+                q.pop() #큐에 들어있는 좌표를 비우는 이유를 잘 모르겠었는데
+                #원래 있던 위치에서 가능한 경로를 모두 탐색하며 가장 가까운 물고기(ft. minHeap)를 먹은 셈이므로 그 이후 경로는 버린다
+                #사실 그게 그냥 bfs임..
+            check = [[False]*n for _ in range(n)] #다음 물고리를 먹기 위해 움직일 때 이미 갔던 곳을 다시 방문할 수 있도록 방문 체크 배열 모두 초기화
+        for dx, dy in (-1,0),(0,-1),(1,0),(0,1): #꼭 문제에서 정해진 우선순위 반영하기!!
             nd, nx, ny = d+1, x+dx, y+dy
             if nx<0 or nx>=n or ny<0 or ny>=n:
                 continue
-            if 0<a[nx][ny] > body or check[nx][ny]: #????
+            if 0<a[nx][ny] > body or check[nx][ny]: #물고기가 존재하는데 물고기 >상어 or 이미 방문한 노드일때는 지나갈수 있는 곳이 아니므로 q에 추가 X
+
                 continue
-            check[nx][ny]=True
-            heappush(q, (nd,nx,ny))
+            check[nx][ny]=True 
+            heappush(q, (nd,nx,ny)) #이동한 거리, 새로 이동한 위치 q에 push
     print(ans)
 
 init()
--- study_16236.py
+++ study_16236.py
@@ -2,8 +2,144 @@
 # 1. 처음 아기 상어의 크기는 2이다.
 # 2. 아기 상어는 자신의 크기보다 큰 물고기가 있는 칸은 지나갈 수 없다.
 # 3. 자신의 크기보다 작은 물고기만 먹을 수 있다.
+
+# 먹을 물고기를 결정하는 것이 핵심
 # 4. 상어에게 가장 가까운 위치에 있는 물고기를 우선순위로 먹어야 한다.
-# 5. 여러마리가 가까이에 있다면, 가장 위쪽에 있는 물고기, 그 다음 순서로 가장 왼쪽에 있는 물고기를 우선순위로 먹어야 한다.
+# 5. 여러마리가 가까이에 있다면, 가장 위쪽에 있는 물고기 (ft.행), 그 다음 순서로 가장 왼쪽에 있는 물고기(ft.열)를 우선순위로 먹어야 한다.
 # -> 4, 5번때문에 minheap에 (이동거리, 행좌표, 열좌표)를 push하는 것
 # 6. 아기 상어는 자신의 크기와 같은 수의 물고기를 먹을 때마다 크기가 1 증가한다.
 
+#
+
+# n = int(input())
+# a = [list(map(int, input().split())) for _ in range(n)]
+# q = []
+
+# def init():
+#     for i in range(n):
+#         for j in range(n):
+#             if a[i][j] == 9:
+#                 heappush(0, i, j)
+#                 a[i][j] = 0
+#                 return
+
+# def bfs():
+#     body,eat,ans = 2,0,0
+#     check = [[False]*n for _ in range(n)]
+
+#     while q:
+#         d,x,y = heappop(q)
+#         if 0 < a[x][y] < body:
+#             eat += 1
+#             a[x][y]=0
+#             if eat == body:
+#                 body+=1
+#                 eat=0
+#             ans += d
+#             d=0
+#             while q:
+#                 q.pop()
+#             check = [[False]*n for _ in range(n)]
+
+#         for dx, dy in (-1,0),(0,-1),(1,0),(0,1):
+#             nd, nx, ny = d+1, x+dx, y+dy
+#             if nx<0 or nx>=n or ny<0 or ny>=n:
+#                 continue
+#             if 0<a[nx][ny]>body or check[nx][ny]:
+#                 continue
+#             check[nx][ny]=True
+#             heappush(q, (nd,nx,ny))
+#     print(ans)
+
+# init()
+# bfs()
+
+
+# n = int(input())
+# a = [list(map(int, input().split())) for _ in range(n)]
+# q = []
+
+# def init():
+#     for i in range(n):
+#         for j in range(n):
+#             if a[i][j] == 9:
+#                 heappush(q, (0, i, j))
+#                 a[i][j] = 0
+#                 return
+
+# def bfs():
+#     body, eat, ans = 2, 0, 0
+
+#     while q:
+#         d, x, y = heappop(q)
+#         if 0<a[x][y]<body:
+#             eat+=1
+#             a[x][y]=0
+#             if eat==body:
+#                 body+=1
+#                 eat=0
+#             ans+=d
+#             d=0
+#             while q:
+#                 q.pop()
+#             check=[[False]*n for _ in range(n)]
+
+#         for dx,dy in (-1,0),(0,-1),(1,0),(0,1):
+#             nd,nx,ny = d+1,x+dx,y+dy
+#             if nx<0 or nx>=n or ny<0 or ny>=n:
+#                 continue
+#             if 0<a[nx][ny]>body or check[nx][ny]:
+#                 continue
+#             check[nx][ny]=True
+#             heappush(q, (nd,nx,ny))
+#     print(ans)
+
+# init()
+# bfs()
+
+from sys import stdin
+from heapq import heappush, heappop
+input = stdin.readline
+
+n=int(input())
+a=[list(map(int, input().split())) for _ in range(n)]
+q=[]
+
+
+def init():
+    for i in range(n):
+        for j in range(n):
+            if a[i][j]==9:
+                heappush(q, (0, i, j))
+                a[i][j]=0
+                return
+
+def bfs():
+    body, eat, ans=2, 0, 0
+    check = [[False]*n for _ in range(n)]
+
+    while q:
+        d, x, y = heappop(q)
+        if (0<a[x][y]<body):
+            eat+=1
+            ans+=d
+            d=0
+            a[x][y]=0
+            if (eat==body):
+                body+=1
+                eat=0
+            while q:
+                q.pop()
+            check=[[False]*n for _ in range(n)]
+        for dx, dy in (-1,0),(0,-1),(1,0),(0,1):
+            nd, nx, ny = d+1, x+dx, y+dy
+            if (nx<0 or nx>=n or ny<0 or ny>=n):
+                continue
+            if (0<a[nx][ny]>body or check[nx][ny]):
+                continue
+            heappush(q, (nd, nx, ny))
+            check[nx][ny]=True
+    print(ans)
+
+init()
+bfs()
