6ee3795b070468a45df13f21855e85dabce6e688
chaekeun <109269974+chaekeun@users.noreply.github.com>
2024-02-17 23:06:00 +0900

    [Silver V] Title: 부분 문자열, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    try:
+        s, t = input().split()
+        i, j = 0, 0
+        while i<len(s) and j<len(t):
+            if s[i]==t[j]:
+                i+=1
+                j+=1
+            else:
+                j+=1
+        if i == len(s):
+            print('Yes')
+        else:
+            print('No')
+    except:
+        break
