7c909bf1c427219eba07a789f63370a8a0e7f0a9
chaekeun <109269974+chaekeun@users.noreply.github.com>
2024-05-20 19:42:27 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,28 +1,30 @@
-# 입력 받기
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
-a.sort()
-
-# 초기화
+a = sorted(list(map(int, input().split())))
 check = [False] * n
 v = []
 s = set()
 
 
 def solve(cnt, idx):
-    if cnt == m:
+    # if 정답이라면: 정답출력 or 저장 등등 
+    if cnt == m: 
         s.add(tuple(v))
         return
-    
+    # for 뱉을 수 있는 모든 자식노드들에 대해서:
     for i in range(idx, n):
+        # if 정답이 유망하다면:
         if not check[i]:
+            # 자식노드로 이동
             check[i] = True
             v.append(a[i])
-            solve(cnt+1, i)
+            # 재귀함수
+            solve(cnt+1, i+1)
+            # 부모노드로 이동 (=이전노드로 돌아간다)
             v.pop()
             check[i] = False
 
 solve(0,0)
 
+# sorted함수를 쓰면 코드길이가 좀 더 줄어든다
 for k in sorted(s):
     print(' '.join(map(str, k)))
