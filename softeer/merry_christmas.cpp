f697d91631bb1e3499e3b8f7fd191067cb3579b4
chaekeun <chaen328@khu.ac.kr>
2025-04-29 09:58:17 +0900

    250429 빡구현: 헷갈림 빠르고/효율적/정확하게 풀어내는 연습 필요

---
+++ softeer/merry_christmas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "##  ##     ##     #####    #####    ##  ##            ##  ##            ##   ##    ##      ####" << endl;
+    cout << "##  ##    ####    ##  ##   ##  ##   ##  ##            ##  ##            ### ###   ####    ##  ##" << endl;
+    cout << "##  ##   ##  ##   ##  ##   ##  ##   ##  ##             ####             #######  ##  ##   ##" << endl;
+    cout << "######   ######   #####    #####     ####               ##     ######   ## # ##  ######    ####" << endl;
+    cout << "##  ##   ##  ##   ##       ##         ##               ####             ##   ##  ##  ##       ##" << endl;
+    cout << "##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##   ##  ##" << endl;
+    cout << "##  ##   ##  ##   ##       ##         ##              ##  ##            ##   ##  ##  ##    ####" ;
+    
+   return 0;
+}
